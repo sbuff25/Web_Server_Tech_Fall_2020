e7e125de3f325ff8077a25ee2dfe773b6ea0d32f
Spencer Buffum <spencerbuffum17@gmail.com>
2020-09-27 15:13:42 -0600

    update

---
+++ Week_6/tablesForBlenderPortfolio/SQLQuery2.sql
@@ -0,0 +1,16 @@
+USE [BlenderPortfolio]
+GO
+
+/****** Object:  Table [dbo].[visitorComments]    Script Date: 9/27/2020 3:12:57 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[visitorComments](
+	[comments] [varchar](250) NULL
+) ON [PRIMARY]
+GO
+
+
---
+++ Week_6/tablesForBlenderPortfolio/SQLQuery3.sql
@@ -0,0 +1,18 @@
+USE [BlenderPortfolio]
+GO
+
+/****** Object:  Table [dbo].[visitorInfo]    Script Date: 9/27/2020 3:13:21 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[visitorInfo](
+	[FirstName] [varchar](50) NULL,
+	[LastName] [varchar](50) NULL,
+	[email] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
