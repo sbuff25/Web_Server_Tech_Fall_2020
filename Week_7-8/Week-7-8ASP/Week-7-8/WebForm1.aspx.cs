24d89c00d671cf986b20b993695e561bcaa3a88a
Spencer Buffum <spencerbuffum17@gmail.com>
2020-10-11 11:46:19 -0600

    update

---
+++ Week_7-8/Week-7-8ASP/Week-7-8/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+namespace Week_7_8
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string myConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+            SqlConnection myConnection;
+
+            myConnection = new SqlConnection(myConnectionString);
+            myConnection.Open();
+
+            string myQuery = "SELECT * FROM visitorComments";
+
+            DataSet myDataSet = new DataSet();
+            SqlCommand myCommand = new SqlCommand(myQuery);
+            myCommand.Connection = myConnection;
+            myCommand.CommandType = CommandType.Text;
+
+            SqlDataAdapter myAdapter = new SqlDataAdapter(myCommand);
+            myAdapter.Fill(myDataSet);
+            myConnection.Close();
+
+            GridView1.DataSource = myDataSet.Tables[0];
+            GridView1.DataBind();
+        }
+    }
+}
