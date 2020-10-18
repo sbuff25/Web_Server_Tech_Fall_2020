1bec24b499d13082592a0c8384a5311122743028
Spencer Buffum <spencerbuffum17@gmail.com>
2020-10-18 14:09:10 -0600

    update

---
+++ Week_9/Sept17MART461/Sept17MART461/Sept17MART461/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sept17MART461
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+            Events myEvents = new Events();
+            // DataSet myDataSet = myEvents.getAllEvents();
+            //DataSet myDataSet = myEvents.getNames();
+            myEvents.insertPerson("Jim", "Jones", "email.mail.com");
+            DataSet myDataSet = myEvents.getNames();
+            //DataSet myDataSet = myEvents.selectIndividualName(1);
+            gvSubjects.DataSource = myDataSet.Tables[0];
+            gvSubjects.DataBind();
+
+    }
+
+        protected void btnCheckboxes_Click(object sender, EventArgs e)
+        {
+           /* string message = "Movies I like: ";
+            bool movie1 = chkMovie1.Checked;
+            bool movie2 = chkMovie2.Checked;
+            bool movie3 = chkMovie3.Checked;
+
+            if(movie1)
+            {
+                message += chkMovie1.Text + "<br />";
+            }
+
+            if (movie2)
+            {
+                message += chkMovie2.Text + "<br />";
+            }
+            if (movie3)
+            {
+                message += chkMovie3.Text + "<br />";
+            }
+
+            lblMovies.Text = message;*/
+        }
+
+        protected void btnFood_Click(object sender, EventArgs e)
+        {
+           
+          /*  if (rbTaco.Checked)
+            {
+                lblFood.Text = "Food I like: " + rbTaco.Text;
+            }
+            else if (rbBurrito.Checked)
+            {
+                lblFood.Text = "Food I like: " + rbBurrito.Text;
+            }
+            else if(rbPizza.Checked)
+            {
+                lblFood.Text = "Food I like: " + rbPizza.Text;
+            }*/
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Week_9/Sept17MART461/Sept17MART461/Sept17MART461/Events.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Sept17MART461
+{
+    public class Events
+    {
+
+        public DataSet getNames()
+        {
+
+            Database myDatabase = new Database();
+            string myQuery = "getNames";
+            DataSet myDataSet = myDatabase.getQueryWithoutParameters(myQuery);
+
+            return myDataSet;
+
+        }
+
+        public DataSet selectIndividualName(int personID)
+        {
+
+            Database myDatabase = new Database();
+            string myQuery = "selectIndividualName";
+            SqlParameter[] myParameters = new SqlParameter[1];
+            myParameters[0] = new SqlParameter("personID", personID);
+            DataSet myDataSet = myDatabase.getQueryWithParameters(myQuery, myParameters);
+
+            return myDataSet;
+
+        }
+        public void insertPerson(string firstName, string lastName, string email)
+        {
+
+            Database myDatabase = new Database();
+            string myQuery = "insertPerson";
+            SqlParameter[] myParameters = new SqlParameter[3];
+            myParameters[0] = new SqlParameter("firstName", firstName);
+            myParameters[1] = new SqlParameter("lastName", lastName);
+            myParameters[2] = new SqlParameter("email", email);
+            myDatabase.executeWithNonQuery(myQuery, myParameters);
+
+           
+
+        }
+
+
+    }
+}
